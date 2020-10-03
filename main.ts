709aa990c2b3210bf5247bf2a2dd569b0b417190
CaptainTNT <72236313+CaptainTNT@users.noreply.github.com>
2020-10-03 00:02:50 -0400

    Update main.ts

--- main.ts
+++ main.ts
@@ -5,6 +5,22 @@ namespace motorWorks {
    //% blockId=Car Spawner
    //% block="Spawn Car"
     export function spawnCar():  void {
+        game.addLife(3)
         let Car = game.createSprite(2, 4)
     }
+    /**
+    * Turns on gass
+    */
+   //% blockId=Car Gass
+   //% block="Turn on Gass"
+    let Gass = 100
+    control.inBackground(function () {
+        while (Gass != 0) {
+            Gass += -1
+            basic.pause(100)
+        }
+        if (Gass == 0) {
+            game.removeLife(1)
+        }
+})
 }
