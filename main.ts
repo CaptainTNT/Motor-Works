5edd2bcb2eba58ba090e68bf8bcd947387007bf2
CaptainTNT <72236313+CaptainTNT@users.noreply.github.com>
2020-10-02 22:51:33 -0400

    Update pxt.json, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,8 @@
-basic.forever(function () {
-	
-})
+namespace motorWorks {
+    /**
+    * Spawns Car
+    */
+   //% blockId=Car Spawner
+   //% block="Spawn Car"
+    let Car = game.createSprite(2, 4)
+}
