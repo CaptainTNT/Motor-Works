3a7b44a997405d42c2ff22009a312ab16212af5d
CaptainTNT <72236313+CaptainTNT@users.noreply.github.com>
2020-10-04 16:58:23 -0400

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,12 +1,20 @@
 namespace motorWorks {
     /**
-    * Spawns Car
+    * Spawns Car with controls 
     */
    //% blockId=Car Spawner
    //% block="Spawn Car"
     export function spawnCar():  void {
         game.addLife(3)
         let Car = game.createSprite(2, 4)
+    input.onButtonPressed(Button.A, function () {
+    Car.change(LedSpriteProperty.X, -1)
+})
+input.onButtonPressed(Button.B, function () {
+    Car.change(LedSpriteProperty.X, 1)
+})
+
+    
     }
     
     /**
