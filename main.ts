ad22d5d078d1bc936114a141d4adc8140dc640f5
CaptainTNT <72236313+CaptainTNT@users.noreply.github.com>
2020-10-07 13:25:39 -0400

    TESTING 123

--- main.ts
+++ main.ts
@@ -1,15 +1,11 @@
 namespace motorWorks {
     let Done = 0
     let Crashed = false
-    let lapsDone = 0
-    function carController(Speed: number) {
-    if (Crashed = true) {
-        Done += Speed
-        }
-    }
+    let Lapsdone = 0
+    let Speed = Math.max(0, 3)
 
-    function mapsLapsDone(Map: number,Laps: number,) {
-    lapsDone = Math.max(0, Laps)
+    function mapsLaps(Map: number,Laps: number,) {
+    Lapsdone = Math.max(0, Laps)
     if (Map = 1) {
 
     }
@@ -22,13 +18,14 @@ namespace motorWorks {
 
     }
 
+    game.gameOver()
     basic.forever(function () {
         if (Crashed = false) {
             if (Done != 100) {
-                Done += 1
+                Done += Speed
                 basic.pause(100)
             } else {
-                lapsDone = lapsDone + 1
+                Lapsdone = Lapsdone + 1
                 Done = 0
             }
         }
@@ -41,17 +38,25 @@ namespace motorWorks {
    //% blockId=Car Spawner
    //% block="Spawn Car"
     export function spawnCar():  void {
-        let Crashed = 2
         game.addLife(3)
         let Car = game.createSprite(2, 4)
+        forever(function () {
+          if (Crashed = false) {
+            let Speed = 1
+            if (Crashed = false) {
+            basic.pause(1050)
+            let Speed = 2
+            }
+          }
+        })
         
     input.onButtonPressed(Button.A, function () {
     Car.change(LedSpriteProperty.X, -1)
     
-})
-input.onButtonPressed(Button.B, function () {
+    })
+    input.onButtonPressed(Button.B, function () {
     Car.change(LedSpriteProperty.X, 1)
-})
+    })
 }
     /**
     * Turns on gass
@@ -80,7 +85,7 @@ input.onButtonPressed(Button.B, function () {
    //% index.min=1 index.max=3
    //% index2.min=1 index2.max=10
     export function choseMAP(index: number, index2: number):  void { 
-        let sprite = game.createSprite(0, 4)
+    let sprite = game.createSprite(0, 4)
     let sprite2 = game.createSprite(0, 3)
     let sprite3 = game.createSprite(0, 2)
     let sprite4 = game.createSprite(0, 1)
@@ -92,15 +97,15 @@ input.onButtonPressed(Button.B, function () {
     let sprite13 = game.createSprite(4, 4)
 
     if (index = 1) {
-        mapsLapsDone(index, index2) //1. straightAway
+        mapsLaps(index, index2) //1. straightAway
     }    
     
     if (index = 2) {
-        mapsLapsDone(index, index2) //2. Oval
+        mapsLaps(index, index2) //2. Oval
     }
     
     if (index = 3) {
-        mapsLapsDone(index, index2)// 3. goCart
+        mapsLaps(index, index2)// 3. goCart
     }
   } 
 } 
--- test.ts
+++ test.ts
@@ -1,2 +1,6 @@
 motorWorks.choseMAP(2, 0)
 motorWorks.spawnCar()
+//CaptainTNT was here :D :)
+//Plz do not type anything in here :D
+//This is were I test :D 
+//https://www.youtube.com/watch?v=dQw4w9WgXcQ :)
